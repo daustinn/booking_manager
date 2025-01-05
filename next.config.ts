7c030489900101ee6a553d472eb79a3cf9a1662d
Daustinn <daustinn.music@gmail.com>
2025-01-05 16:20:10 -0500

    feat: Add environment variables for Cloudinary and base URL
    Add environment variables for Cloudinary and base URL in the next.config.ts file.
    This allows the application to access the Cloudinary URL and the base URL for API calls.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  env: {
+    CLOUDINARY_URL: process.env.CLOUDINARY_URL,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL
+  }
+}
 
-export default nextConfig;
+export default nextConfig
